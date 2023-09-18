5a2bd173af14a1bc3b1d699f989e84b3cbb9d578
Adriana Perez <adriana.107a@gmail.com>
2023-09-18 14:32:26 -0400

    matplotlib

---
+++ plt.py
@@ -0,0 +1,11 @@
+import codecademylib3_seaborn
+import random 
+from matplotlib import pyplot as plt
+# Add your code below:
+
+numbers_a = range(1, 13)
+numbers_b = random.sample(range(1000), 12)
+
+plt.plot(numbers_a, numbers_b)
+
+plt.show()
